392f65baf458f90483491f709da73c528f824904
emmanuel <emmanuel.ramirez@correounivalle.edu.co>
2020-05-03 16:21:03 -0500

    Started tutorial.

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
 
-print("HEllo")
+import time
+
+start = time.perf_counter()
+
+
+def do_something():
+    print('Sleeping 1 second...')
+    time.sleep(1)
+    print('Done Sleeping')
+
+do_something()
+
+finish = time.perf_counter()
+
+print(f'Finisged in {round(finish-start, 2)} second(s)')    

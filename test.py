0b90d2191f3d53b42af92e80f362f955ab7a48cc
emmanuel <emmanuel.ramirez@correounivalle.edu.co>
2020-05-03 17:45:31 -0500

    added

--- test.py
+++ test.py
@@ -19,9 +19,4 @@ p2.start()
 
 finish = time.perf_counter()
 
-<<<<<<< HEAD
 print(f'Finisged in {round(finish-start, 4)} second(s)')
-=======
-
-print(f'Finisged in {round(finish-start, 2)} second(s)')
->>>>>>> b131b730a07452bebe8bbb5f70312605ce1e160b

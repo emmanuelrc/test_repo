68a841fcb9967c8f531cbae20fb344342cc66f13
emmanuel <emmanuel.ramirez@correounivalle.edu.co>
2020-05-03 19:31:20 -0500

    123

--- process-images.py
+++ process-images.py
@@ -1,5 +1,6 @@
 
 import time
+import concurrent.futures
 from PIL import Image, ImageFilter
 
 img_names = [
@@ -24,7 +25,7 @@ t1 = time.perf_counter()
 
 size = (1200, 1200)
 
-for img_name in img_names:
+def process_image(img_name):
     img = Image.open(img_name)
 
     img = img.filter(ImageFilter.GaussianBlur(15))
@@ -32,6 +33,9 @@ for img_name in img_names:
     img.thumbnail(size)
     img.save(f'processed/{img_name}')
     print(f'{img_name} was processed...')
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    executor.map(process_image, img_names)
 
 t2 = time.perf_counter()
 
